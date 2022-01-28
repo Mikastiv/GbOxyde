c544faaaad911900e2ae1341f315510465887b59
mikastiv <mikastiv@outlook.com>
2022-01-28 01:27:01 -0500

    more inst

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -57,9 +57,9 @@ impl Cpu {
     }
 
     fn imm(&mut self, bus: &mut impl Interface) -> u8 {
-        let data = bus.read(self.regs.pc);
+        let value = bus.read(self.regs.pc);
         self.inc_pc();
-        data
+        value
     }
 
     fn imm_word(&mut self, bus: &mut impl Interface) -> u16 {
@@ -68,11 +68,11 @@ impl Cpu {
         u16::from_le_bytes([lo, hi])
     }
 
-    fn push(&mut self, bus: &mut impl Interface, data: u16) {
+    fn push(&mut self, bus: &mut impl Interface, value: u16) {
         self.regs.dec_sp();
-        bus.write(self.regs.sp, (data >> 8) as u8);
+        bus.write(self.regs.sp, (value >> 8) as u8);
         self.regs.dec_sp();
-        bus.write(self.regs.sp, data as u8);
+        bus.write(self.regs.sp, value as u8);
     }
 
     fn pop(&mut self, bus: &mut impl Interface) -> u16 {
@@ -122,13 +122,13 @@ impl Cpu {
         S: Copy,
         Self: Dst<D> + Src<S>,
     {
-        let data = self.read(bus, src);
-        self.write(bus, dst, data);
+        let value = self.read(bus, src);
+        self.write(bus, dst, value);
     }
 
     fn ld_d16(&mut self, bus: &mut impl Interface, dst: Reg16) {
-        let data = self.imm_word(bus);
-        self.regs.write16(dst, data);
+        let value = self.imm_word(bus);
+        self.regs.write16(dst, value);
     }
 
     fn ld_sp_hl(&mut self, bus: &mut impl Interface) {
@@ -138,33 +138,231 @@ impl Cpu {
 
     fn ld_mem_d16_sp(&mut self, bus: &mut impl Interface) {
         let address = self.imm_word(bus);
-        let data = self.regs.sp;
-        bus.write(address, data as u8);
-        bus.write(address.wrapping_add(1), (data >> 8) as u8);
+        let value = self.regs.sp;
+        bus.write(address, value as u8);
+        bus.write(address.wrapping_add(1), (value >> 8) as u8);
     }
 
     fn ld_hl_sp_d8(&mut self, bus: &mut impl Interface) {
-        let data = self.imm(bus) as i8 as i16 as u16;
+        let value = self.imm(bus) as i8 as i16 as u16;
         let sp = self.regs.sp;
 
-        let carry = (sp & 0x00FF) + (data & 0x00FF) > 0x00FF;
-        let half_carry = (sp & 0x000F) + (data & 0x000F) > 0x000F;
+        let carry = (sp & 0x00FF) + (value & 0x00FF) > 0x00FF;
+        let half_carry = (sp & 0x000F) + (value & 0x000F) > 0x000F;
         self.regs.set_flags(Flags::C, carry);
         self.regs.set_flags(Flags::H, half_carry);
         self.regs.set_flags(Flags::Z | Flags::N, false);
 
-        self.regs.set_hl(sp.wrapping_add(data));
+        self.regs.set_hl(sp.wrapping_add(value));
         bus.tick(1);
     }
 
     fn push16(&mut self, bus: &mut impl Interface, src: Reg16) {
         bus.tick(1);
-        let data = self.regs.read16(src);
-        self.push(bus, data);
+        let value = self.regs.read16(src);
+        self.push(bus, value);
     }
 
     fn pop16(&mut self, bus: &mut impl Interface, dst: Reg16) {
-        let data = self.pop(bus);
-        self.regs.write16(dst, data);
+        let value = self.pop(bus);
+        self.regs.write16(dst, value);
+    }
+
+    fn alu_add(&mut self, value: u8, cy: bool) -> u8 {
+        let a = self.regs.a;
+        let cy = cy as u8;
+        let result = u16::from(a) + u16::from(value) + u16::from(cy);
+
+        let carry = result > 0xFF;
+        let half_carry = (a & 0x0F) + (value & 0x0F) + cy > 0x0F;
+        self.regs.set_flags(Flags::Z, result as u8 == 0);
+        self.regs.set_flags(Flags::N, false);
+        self.regs.set_flags(Flags::H, half_carry);
+        self.regs.set_flags(Flags::C, carry);
+
+        result as u8
+    }
+
+    fn add<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        self.regs.a = self.alu_add(value, false);
+    }
+
+    fn add16(&mut self, bus: &mut impl Interface, src: Reg16) {
+        let value = self.regs.read16(src);
+        let hl = self.regs.hl();
+
+        let (result, carry) = hl.overflowing_add(value);
+        let half_carry = (hl & 0x0FFF) + (value & 0x0FFF) > 0x0FFF;
+        self.regs.set_flags(Flags::C, carry);
+        self.regs.set_flags(Flags::N, false);
+        self.regs.set_flags(Flags::H, half_carry);
+
+        self.regs.set_hl(result);
+        bus.tick(1);
+    }
+
+    fn add_sp_d8(&mut self, bus: &mut impl Interface) {
+        let value = self.imm(bus) as i8 as i16 as u16;
+        let sp = self.regs.sp;
+
+        let carry = (sp & 0x00FF) + (value & 0x00FF) > 0x00FF;
+        let half_carry = (sp & 0x000F) + (value & 0x000F) > 0x000F;
+        self.regs.set_flags(Flags::C, carry);
+        self.regs.set_flags(Flags::H, half_carry);
+        self.regs.set_flags(Flags::Z | Flags::N, false);
+
+        self.regs.sp = sp.wrapping_add(value);
+        bus.tick(2);
+    }
+
+    fn adc<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        self.regs.a = self.alu_add(value, self.regs.c());
+    }
+
+    fn alu_sub(&mut self, value: u8, cy: bool) -> u8 {
+        let a = self.regs.a;
+        let cy = cy as u8;
+        let result = a.wrapping_sub(value).wrapping_sub(cy);
+
+        let carry = u16::from(a) < u16::from(value) + u16::from(cy);
+        let half_carry = (a & 0x0F) < (value & 0x0F) + (cy & 0x0F);
+        self.regs.set_flags(Flags::Z, result == 0);
+        self.regs.set_flags(Flags::N, true);
+        self.regs.set_flags(Flags::H, half_carry);
+        self.regs.set_flags(Flags::C, carry);
+
+        result
+    }
+
+    fn sub<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        self.regs.a = self.alu_sub(value, false);
+    }
+
+    fn sbc<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        self.regs.a = self.alu_sub(value, self.regs.c());
+    }
+
+    fn and<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        let result = self.regs.a & value;
+
+        self.regs.set_flags(Flags::Z, result == 0);
+        self.regs.set_flags(Flags::N | Flags::C, false);
+        self.regs.set_flags(Flags::H, true);
+
+        self.regs.a = result;
+    }
+
+    fn or<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        let result = self.regs.a | value;
+
+        self.regs.set_flags(Flags::Z, result == 0);
+        self.regs.set_flags(Flags::N | Flags::C | Flags::H, false);
+
+        self.regs.a = result;
+    }
+
+    fn xor<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        let result = self.regs.a ^ value;
+
+        self.regs.set_flags(Flags::Z, result == 0);
+        self.regs.set_flags(Flags::N | Flags::C | Flags::H, false);
+
+        self.regs.a = result;
+    }
+
+    fn cp<I, S>(&mut self, bus: &mut I, src: S)
+    where
+        I: Interface,
+        S: Copy,
+        Self: Src<S>,
+    {
+        let value = self.read(bus, src);
+        self.alu_sub(value, false);
+    }
+
+    fn inc<I, D>(&mut self, bus: &mut I, dst: D)
+    where
+        I: Interface,
+        D: Copy,
+        Self: Dst<D> + Src<D>,
+    {
+        let value = self.read(bus, dst);
+        let result = value.wrapping_add(1);
+
+        self.regs.set_flags(Flags::Z, result == 0);
+        self.regs.set_flags(Flags::N, false);
+        self.regs.set_flags(Flags::H, (value & 0x0F) + 0x01 > 0x0F);
+
+        self.write(bus, dst, result);
+    }
+
+    fn inc16(&mut self, bus: &mut impl Interface, dst: Reg16) {
+        let value = self.regs.read16(dst);
+        self.regs.write16(dst, value.wrapping_add(1));
+        bus.tick(1);
+    }
+
+    fn dec<I, D>(&mut self, bus: &mut I, dst: D)
+    where
+        I: Interface,
+        D: Copy,
+        Self: Dst<D> + Src<D>,
+    {
+        let value = self.read(bus, dst);
+        let result = value.wrapping_sub(1);
+
+        self.regs.set_flags(Flags::Z, result == 0);
+        self.regs.set_flags(Flags::N, false);
+        self.regs.set_flags(Flags::H, (value & 0x0F) + 0x01 > 0x0F);
+
+        self.write(bus, dst, result);
+    }
+
+    fn dec16(&mut self, bus: &mut impl Interface, dst: Reg16) {
+        let value = self.regs.read16(dst);
+        self.regs.write16(dst, value.wrapping_sub(1));
+        bus.tick(1);
     }
 }
